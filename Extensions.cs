684b1765d025b0b61dc3a85f5235562c399623c2
Kyle Lawrence <lawrencekyle@live.com>
2024-03-24 14:56:53 -0600

    Swap to maybe class

--- Extensions.cs
+++ Extensions.cs
@@ -39,12 +39,12 @@ public static class Extensions
     private static readonly int[] fieldValues = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216];
     public static bool FieldIsBoosted(this Card card, int index)
     {
-        if (index < 0 || index > 24) return false;
+        if (index < 0 || index > 24 || index == 12) return false;
         return (card.Boosts & fieldValues[index]) != 0;
     }
     public static Card ToggleBoost(this Card card, int index)
     {
-        if (index < 0 || index > 24) return card;
+        if (index < 0 || index > 24 || index == 12) return card;
         var newBoosts = card.Boosts ^ fieldValues[index];
         return new(
             card.Id,
